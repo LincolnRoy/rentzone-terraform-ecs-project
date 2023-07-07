bafd15782283cd6553e9b7d1fe306481b5804828
Lincoln Roy <roylincoln002@gmail.com>
2023-07-07 16:39:56 -0400

    created vpc

--- variables.tf
+++ variables.tf
@@ -15,3 +15,45 @@ variable "environment" {
   description  = "environment"
   type         = string
 }
+
+# vpc variables
+variable "vpc_cidr" {
+  description  = "vpc cidr block"
+  type         = string
+}
+
+# subnet variables
+variable "public_subnet_az1_cidr" {
+  description  = "public subnet az1 cidr block"
+  type         = string
+}
+
+# subnet variables
+variable "public_subnet_az2_cidr" {
+  description  = "public subnet az2 cidr block"
+  type         = string
+}
+
+# private subneta az1 variables
+variable "private_subnet_app_az1" {
+  description  = "private subnet app az1 cidr block"
+  type         = string
+}
+
+# private subneta az2 variables
+variable "private_subnet_app_az2" {
+  description  = "private subnet app az2 cidr block"
+  type         = string
+}
+
+# private subneta az2 variables
+variable "private_subnet_db_az1" {
+  description  = "private subnet db az1 cidr block"
+  type         = string
+}
+
+# private subneta az2 variables
+variable "private_subnet_db_az2" {
+  description  = "private subnet db az2 cidr block"
+  type         = string
+}
